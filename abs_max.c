de9ffe27cf6c85cf53508fc476a762d230a3c779
Александр Ширинкин <89913639+RetiredTea@users.noreply.github.com>
2023-10-18 12:38:39 +0300

    Add files via upload

--- abs_max.c
+++ abs_max.c
@@ -1,6 +1,6 @@
 int abs_max(int q[], int len_q){
-	int q_max = 0, max_i;
-	for(int i = 0; i < len_q;i++){
+	int q_max = 0, max_i , i = 0;
+	for(i ; i < len_q;i++){
         if (abs(q[i]) > q_max){
 		q_max = abs(q[i]);
 		max_i = i;
